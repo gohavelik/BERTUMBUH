51f7f6ae903f8903b2f2ef2e06bcc52b28f73df2
gohavelik <gohavelik@gmail.com>
2023-03-05 11:44:22 +0100

    121

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const menu = document.querySelector(".top-menu");
+const arrow = document.querySelector(".menu-arrow")
+
+
 const menuArrow = document
-.querySelector(".menu-arrow")
+.querySelector(".menu-programs")
 .addEventListener("click", function() {
-    document.querySelector(".top-menu").style.display = "none";
+    if (menu.id == "closed") {
+       menu.style = "display:flex;"
+       menu.id = "opened";
+       arrow.classList.add("arrow-animation");
+
+    } 
+    else if (menu.id == "opened") {
+        menu.style = "display:none";
+        menu.id = "closed"
+        arrow.classList.remove("arrow-animation");
+    }
+    
 })
